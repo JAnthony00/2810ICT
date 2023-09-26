696aeda1b96109d36c3ddffb4c22f6b11d66e640
JAnthony00 <jack.brighton@griffithuni.edu.au>
2023-09-26 14:26:15 +1000

    --Commit 5-- Rewrote accidents in each hour of the day. Now works returning a dictionary for instances of hours in 24HR time.
    --Commit 4--
    Added comments.
    
    --Commit 3--
    Added a Test File for future Unit Tests.
    created getAverageDataHour - work in progress to get average hour when accidents happen.
    plotted data for visual referencing while devving

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ One other "Insight" of your choice.
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class TableData:
     def __init__(self, fileName):
         # reads the chosen file into a dataframe
@@ -27,33 +28,36 @@ class TableData:
         # --- for a user selected period, display the information of all accidents that happened in the period ---
 
         # define the date range
-        searchStart = pd.to_datetime(startDate, format= '%d/%m/%Y') # format the data into d/m/y because americans
-        searchEnd = pd.to_datetime(endDate, format= '%d/%m/%Y')
+        searchStart = pd.to_datetime(startDate, format='%d/%m/%Y')  # format the data into d/m/y because americans
+        searchEnd = pd.to_datetime(endDate, format='%d/%m/%Y')
 
         # convert to timestamp format
-        self.data['ACCIDENT_DATE'] = pd.to_datetime(self.data['ACCIDENT_DATE'], format= '%d/%m/%Y')
+        self.data['ACCIDENT_DATE'] = pd.to_datetime(self.data['ACCIDENT_DATE'], format='%d/%m/%Y')
 
         # selected data is between start and end data
-        selectedData = self.data[(self.data['ACCIDENT_DATE'] >= searchStart) & (self.data['ACCIDENT_DATE'] <= searchEnd)]
+        selectedData = self.data[
+            (self.data['ACCIDENT_DATE'] >= searchStart) & (self.data['ACCIDENT_DATE'] <= searchEnd)]
 
         return selectedData
 
-    def getAverageDataHour(self):
+    def getAccidentPerHour(self):
+        # --- For a user selected period, produce a chart to show the number of accidents in each hour of the day ---
 
-        # make averaged hour within filtered time dataframe
-        self.data = self.getSelectedDateData(startDate, endDate)
+        self.data = self.getSelectedDateData(startDate, endDate)  # dataframe is the filtered time period
 
-        # get the totals for each hour an accident happens in that time
-        entryCounts = {}
+        hourCount = {}  # create a dictionary for the counted accidents
 
-        for entry in self.data['ACCIDENT_TIME']:
-            if entry in entryCounts:
-                entryCounts[entry] += 1
+        for accidentTime in self.data['ACCIDENT_TIME']:
+            hour = accidentTime.split('.')[0]  # data formatted as HH.MM.SS so split at '.'
+
+            print(hour)
+
+            if hour in hourCount:
+                hourCount[hour] += 1  # if the current hour is in the dictionary, add 1 to it
             else:
-                entryCounts[entry] = 1
+                hourCount[hour] = 1  # otherwise give it a value of 1
 
-        return entryCounts
-
+        return hourCount
 
     def getSelectedType(self, accidentType):
         # --- for a user selected period, retrieve all accidents caused by a type containing a keyword ---
@@ -66,13 +70,14 @@ class TableData:
 
         return searchType
 
-    def getAlcoholInvolved(self, hasAlcohol): # get data where alcohol was involved in the accident
+    def getAlcoholInvolved(self, hasAlcohol):  # get data where alcohol was involved in the accident
         searchAlcohol = self.data[self.data['ALCOHOLTIME'].str.lower().str.contains(hasAlcohol.lower())]
 
         return searchAlcohol
 
+
 # create an instance of the TableData
-table = TableData('Stats.csv')
+table = TableData('WorkableStats.csv')
 
 # data for selected range -- this data will be received from UI selection
 startDate = '1/07/2013'
@@ -82,7 +87,7 @@ endDate = '7/07/2013'
 accidentType = 'Struck Pedestrian'
 hasAlcohol = 'Yes'
 
-
 print("Table Data: ")
 # print(table.getSelectedType(accidentType))
-print(table.getAlcoholInvolved(hasAlcohol))
+# print(table.getAlcoholInvolved(hasAlcohol))
+table.getAccidentPerHour()
